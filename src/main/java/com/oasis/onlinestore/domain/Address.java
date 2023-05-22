087ab931525c1caee3df8c1016cf434a1953584c
feventewodros <ftewodros@miu.edu>
2023-05-21 21:36:03 -0500

    address update

--- src/main/java/com/oasis/onlinestore/domain/Address.java
+++ src/main/java/com/oasis/onlinestore/domain/Address.java
@@ -3,9 +3,9 @@ package com.oasis.onlinestore.domain;
 import jakarta.persistence.Entity;
 import org.springframework.data.annotation.Id;
 
-@Entity
+
 public class Address {
-@Id
+
     private String city;
     private String street;
     private String zip;
--- src/main/java/com/oasis/onlinestore/domain/AddressType.java
+++ src/main/java/com/oasis/onlinestore/domain/AddressType.java
@@ -3,10 +3,10 @@ package com.oasis.onlinestore.domain;
 import jakarta.persistence.Entity;
 import org.springframework.data.annotation.Id;
 
-@Entity
+
 public class AddressType {
 
-    @Id
+
     private int id;
     private String type;
 }

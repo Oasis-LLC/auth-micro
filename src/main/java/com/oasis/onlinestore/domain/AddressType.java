22b3a3b732a5c6f115fbae74b8dccb9c6a25cfb5
Yeabsra Hailu <yeabsdebe@gmail.com>
2023-05-22 16:12:11 -0500

    Changed AddressType from class to enum

--- src/main/java/com/oasis/onlinestore/domain/AddressType.java
+++ src/main/java/com/oasis/onlinestore/domain/AddressType.java
@@ -1,22 +1,6 @@
 package com.oasis.onlinestore.domain;
 
 
-
-import jakarta.persistence.Embeddable;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-
-@Embeddable
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
-
-public class AddressType {
-
-    @Id
-
-    private int id;
-    private String type;
+public enum AddressType {
+    BILLING,SHIPPING
 }

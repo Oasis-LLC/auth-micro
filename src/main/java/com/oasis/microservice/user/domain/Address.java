baea34ac31333ec53a11d5468d3802238cd0d942
Yeabsra Hailu <yeabsdebe@gmail.com>
2023-05-23 12:38:42 -0500

    Chore: imports

--- src/main/java/com/oasis/microservice/user/domain/Address.java
+++ src/main/java/com/oasis/microservice/user/domain/Address.java
@@ -1,7 +1,10 @@
 package com.oasis.microservice.user.domain;
 
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
